9c70dfd8802b47c2ff879846f6c19882cfcdaaf1
Juan Esteban Marin <62470801+imaes11st@users.noreply.github.com>
2021-07-08 00:04:43 -0500

    Creando pantalla de usuario

--- screens/CreateUserScreen.js
+++ screens/CreateUserScreen.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react'
+import { View, Button, TextInput, ScrollView, StyleSheet } from 'react-native';
 
 const CreateUserScreen = () => {
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        phone: ''
+    });
+
+    const handleChangeText = (name, value) =>{
+        setState({ ...state, [name]: value})
+    };
+    
     return(
-        <div>
-            <Text>Users Create</Text>
-        </div>
-    )
-}
+        <ScrollView style={StyleSheet.container}>
+            <view style={StyleSheet.inputGroup}>
+                <TextInput placeholder="Nombre usuario"
+                onChangeText={(value) => handleChangeText('name', value)}
+                />
+            </view>
+            <view style={StyleSheet.inputGroup}>
+                <TextInput placeholder="Email"
+                 onChangeText={(value) => handleChangeText('email', value)}
+                 />
+            </view>
+            <view style={StyleSheet.inputGroup}>
+                <TextInput placeholder="Numero"
+                onChangeText={(value) => handleChangeText('phone', value)}
+                />
+            </view>
+            <view>
+                <Button tittle="Guardar Usuario" onPress={() => console.log(state)}/>
+            </view>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:35,
+    },
+    inputGroup:{
+        flex:1,
+        padding:0,
+        marginBottom:15,
+        borderBottomWidth:1,
+        borderBottomColor:'#cccccc',
+    },
+});
 
 export default CreateUserScreen
